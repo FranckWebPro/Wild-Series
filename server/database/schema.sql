2e162400fd6e362fcadf93f472c2bfb7e5181c5f
Franck <contact@franckwebpro.com>
2024-05-25 17:40:45 +0200

    udpate dotenv and database

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,19 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+CREATE TABLE category (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(100)
+);
+
+CREATE TABLE program (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    synopsis TEXT NOT NULL,
+    poster VARCHAR(250) NULL,
+    country VARCHAR(100) NOT NULL,
+    release_year INT NOT NULL,
+    category_id INT NOT NULL,
+    Foreign Key (category_id) REFERENCES category(id)
+);
